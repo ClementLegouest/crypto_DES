3408264896120c59af20e5d080da4ff658e04622
ClementLegouest <clement.legouest@gmail.com>
2020-01-26 21:44:49 +0100

    [evo] utility lib

---
+++ utils.py
@@ -0,0 +1,14 @@
+##############################################################################
+#
+# Clément Legouest
+#
+# Functions used to crypt and decrypt DES
+#
+# Managed to be used under Python 3.8.0 64 bits
+#
+##############################################################################
+
+
+def display_dict(this_dict):
+    for x, y in this_dict.items():
+        print(x, y)
